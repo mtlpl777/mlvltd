65b2b3ab2a13eb9a58fa0558eab85e8be8ba5e55
mtlpl777 <77997946+mtlpl777@users.noreply.github.com>
2021-03-24 12:56:19 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+
+
+
+  function popup (mainLogo, windowname) {
+    if (!window.focus) return true;
+    var href;
+    if (typeof(mainLogo) === 'string') href=mylink;
+    else href=mylink.href;
+    window.open(href, windowname, 'width=400, height=200, scrollbars=yes');
+    return false;
+  }
+
+  // Popup
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
